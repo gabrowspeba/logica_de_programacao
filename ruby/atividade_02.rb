e9f3f15504a93fcfd289dd7c73690a788efba0bc
Gabriel Barbosa <93681453+gabrowspeba@users.noreply.github.com>
2021-12-10 14:56:47 -0300

    Acrescentando complexidade ao arquivo atividade_02.rb

---
+++ ruby/atividade_02.rb
@@ -0,0 +1,13 @@
+print "Informe o seu nome: "
+
+name = gets.chomp
+
+print "Informe o seu sobrenome: "
+
+surename = gets.chomp
+
+print "Informe a sua idade: "
+
+age = gets.chomp.to_i
+
+puts "Olá, #{name} #{surename}. Você tem #{age}! =]"

81781482be9cf73ee1974dce4b3f239780bdc569
Gabriel Barbosa <93681453+gabrowspeba@users.noreply.github.com>
2021-12-10 14:46:02 -0300

    Acrescentando arquivo soma_int

---
+++ ruby/soma_int.rb
@@ -0,0 +1,13 @@
+print "Essa é uma calculadora de dois números inteiros! =]"
+
+print "Digite o primeiro número inteiro: "
+
+num1 = gets.chomp.to_i
+
+print "Digite o segundo número inteiro: "
+
+num2 = gets.chomp.to_i
+
+addition = num1 + num2
+
+puts "O resultado da adição entre os dois números inteiros informados é #{addition}."
